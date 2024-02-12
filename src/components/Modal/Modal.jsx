bb8ca9f9a33e8e4589631eb83e758193ddcd281b
AliievLomachMaxim <aliev.lomach.maxim@gmail.com>
2024-02-12 22:05:39 +0200

    add less

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,27 +1,73 @@
-const Modal = ({ children, hideModal, toggleModal }) => {
-	return (
-		<div
-			className='modal fade show'
-			style={{ display: 'block', backdropFilter: 'blur(5px)' }}
-			onClick={toggleModal}
-		>
-			<div className='modal-dialog'>
-				<div className='modal-content'>
-					<div className='modal-header'>
-						<h5 className='modal-title'> Modal</h5>
-						<button
-							type='button'
-							className='btn-close'
-							aria-label='Close'
-							// onClick={() => toggleModal(false)}
-							onClick={toggleModal}
-						></button>
+import { Component } from 'react'
+
+class Modal extends Component {
+	handleClickEsc = (e) => {
+		console.log('click Esc')
+		if (e.code === 'Escape') {
+			this.props.toggleModal()
+		}
+	}
+	componentDidMount() {
+		document.addEventListener('keydown', this.handleClickEsc)
+	}
+
+	componentWillUnmount() {
+		document.removeEventListener('keydown', this.handleClickEsc)
+	}
+
+	render() {
+		const { children, toggleModal } = this.props
+		return (
+			<div
+				className='modal fade show'
+				style={{ display: 'block', backdropFilter: 'blur(5px)' }}
+				// onClick={toggleModal}
+			>
+				<div className='modal-dialog'>
+					<div className='modal-content'>
+						<div className='modal-header'>
+							<h5 className='modal-title'> Modal</h5>
+							<button
+								type='button'
+								className='btn-close'
+								aria-label='Close'
+								// onClick={() => toggleModal(false)}
+								onClick={toggleModal}
+							></button>
+						</div>
+						<div className='modal-body'>{children}</div>
 					</div>
-					<div className='modal-body'>{children}</div>
 				</div>
 			</div>
-		</div>
-	)
+		)
+	}
 }
 
 export default Modal
+// const Modal = ({ children, hideModal, toggleModal }) => {
+// 	return (
+// 		<div
+// 			className='modal fade show'
+// 			style={{ display: 'block', backdropFilter: 'blur(5px)' }}
+// 			onClick={toggleModal}
+// 		>
+// 			<div className='modal-dialog'>
+// 				<div className='modal-content'>
+// 					<div className='modal-header'>
+// 						<h5 className='modal-title'> Modal</h5>
+// 						<button
+// 							type='button'
+// 							className='btn-close'
+// 							aria-label='Close'
+// 							// onClick={() => toggleModal(false)}
+// 							onClick={toggleModal}
+// 						></button>
+// 					</div>
+// 					<div className='modal-body'>{children}</div>
+// 				</div>
+// 			</div>
+// 		</div>
+// 	)
+// }
+
+// export default Modal
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,42 +3,67 @@ import CreateProductForm from '../Forms/CreateProductForm'
 import ProductList from '../ProductList/ProductList'
 import data from '../../data.json'
 import { nanoid } from 'nanoid'
-import CreateProductFormWithFormik from '../Forms/CreateProductFormWithFormik'
+// import CreateProductFormWithFormik from '../Forms/CreateProductFormWithFormik'
 
 class Products extends Component {
 	state = {
-		products: data,
+		products: null,
+		// products: [],
+		test: 123,
+		counter: { count: 0 },
+	}
+
+	componentDidMount() {
+		const localData = localStorage.getItem('products')
+		if (localData) {
+			this.setState({ products: JSON.parse(localData) })
+		} else {
+			this.setState({ products: data })
+		}
+	}
+
+	componentDidUpdate(prevProps, prevState) {
+		if (prevState.products?.length !== this.state.products.length) {
+			localStorage.setItem('products', JSON.stringify(this.state.products))
+		}
+
+		// if (prevState.products.length > this.state.products.length) {
+		// 	console.log('successfully deleted product!')
+		// }
+		// if (prevState.products.length < this.state.products.length) {
+		// 	console.log('successfully added product!')
+		// }
 	}
 
 	handleDelete = (id) => {
 		this.setState((prev) => ({
 			products: prev.products.filter((el) => el.id !== id),
+			counter: { count: prev.counter.count + 1 },
 		}))
 	}
 
 	createProduct = (data) => {
-		// ...some
 		const id = nanoid()
-		// newData.id = id
-		// console.log('newData :>> ', newData)
-		// this.setState((prev) => ({
-		// 	products: [newData, ...prev.products],
-		// }))
 
 		const newData = { ...data, id }
 		this.setState((prev) => ({
 			products: [newData, ...prev.products],
-			// products: prev.products.push(newData),
 		}))
-		// this.state.products.push(newData)
 	}
 	render() {
-		console.log('this.state :>> ', this.state)
+		// console.log('this.state :>> ', this.state)
 		return (
 			<>
-				<CreateProductFormWithFormik createProduct={this.createProduct} />
-				{/* <CreateProductForm createProduct={this.createProduct} /> */}
-				<ProductList handleDelete={this.handleDelete} products={this.state.products} />
+				<button
+					onClick={() => this.setState((prev) => ({ counter: { count: prev.counter.count + 1 } }))}
+				>
+					click
+				</button>
+				{/* <CreateProductFormWithFormik createProduct={this.createProduct} /> */}
+				<CreateProductForm createProduct={this.createProduct} counter={this.state.counter} />
+				{this.state.products && (
+					<ProductList handleDelete={this.handleDelete} products={this.state.products} />
+				)}
 			</>
 		)
 	}
